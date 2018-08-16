41c4c592167d41b0af9907cae0f5d60aaa00935b
Nick Rempel <nbrempel@gmail.com>
2018-08-15 20:25:59 -0700

    Update slash.rb

--- slash.rb
+++ slash.rb
@@ -1,7 +1,7 @@
 class Slash < Formula
   desc "Slack terminal client written in Swift."
   homepage "https://github.com/slash-hq/slash"
-  url "https://github.com/slash-hq/slash/archive/0.1.0.tar.gz"
+  url "https://github.com/nrempel/slash/archive/0.1.0.tar.gz"
   sha256 "8a709579ffba7c47b1e1975bb418d72ecbd542539d4cd6f7a72d876808bfbdb2"
 
   depends_on :xcode
